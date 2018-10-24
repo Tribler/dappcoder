07d5736504e5af610011ad07b4592ce04a8a133f
Martijn de Vos <mdmartijndevos@gmail.com>
2018-10-24 13:50:02 +0200

    Made DAppCrowd community, integrated IPv8

---
+++ dappcrowd/community.py
@@ -0,0 +1,21 @@
+from binascii import unhexlify
+
+from pyipv8.ipv8.attestation.trustchain.community import TrustChainCommunity
+from pyipv8.ipv8.deprecated.community import Community
+from pyipv8.ipv8.peer import Peer
+
+
+class DAppCrowdTrustchainCommunity(TrustChainCommunity):
+    master_peer = Peer(unhexlify('3081a7301006072a8648ce3d020106052b81040027038192000404267964a5be4a43ee1e59397c6765'
+                                 '8db0dadc276a89163a3b1f7dec3fdb4cecd94dd80968c9983bfffcd2cd58e8ec7ada6dded7ff3b389b'
+                                 '85f691ee0e7981326b4b4deb80ad536801d781795a335f501b80c00b479f076f0384fa7fa3bd940f76'
+                                 '82840dae77d46f938f49743acb5d2ab723046982608d60f2398853f9898d97e4e3b35fa19eb92f0ba2'
+                                 'c1570a31ae72'))
+
+
+class DAppCrowdCommunity(Community):
+    master_peer = Peer(unhexlify('3081a7301006072a8648ce3d020106052b81040027038192000406297d96eafe1f25408ecc44062310'
+                                 '67d4d644bf837e051d64fee582788544b360d30f21004eeb7f3425331423c7d5c9cc56ad7358558a43'
+                                 '6fd46ac53dc9f25575f4b28a512c8ca002aaab6d820800634f009a8d509e600a9c7f9a171e9d0c3a66'
+                                 'd2a823a5f6d6d2bfb5d96c1725163b03242a1e6b7d51ae110d5666d696640f4e3633bd9da346397dcd'
+                                 '0dd47bd6fe29'))
---
+++ twisted/plugins/dappcrowd.py
@@ -0,0 +1,157 @@
+"""
+This twistd plugin starts a TrustChain crawler.
+"""
+from __future__ import absolute_import
+
+import os
+import signal
+import sys
+
+import logging
+
+from dappcrowd.community import DAppCrowdCommunity, DAppCrowdTrustchainCommunity
+from pyipv8.ipv8.peerdiscovery.discovery import RandomWalk
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from twisted.application.service import MultiService, IServiceMaker
+from twisted.internet import reactor
+from twisted.plugin import IPlugin
+from twisted.python import usage
+from twisted.python.log import msg
+from zope.interface import implements
+
+from pyipv8.ipv8_service import IPv8
+from pyipv8.ipv8.attestation.trustchain.settings import TrustChainSettings
+from pyipv8.ipv8.REST.rest_manager import RESTManager
+
+
+class Options(usage.Options):
+    optParameters = [
+        ["statedir", "s", ".", "Use an alternate statedir", str],
+        ["apiport", "p", 8085, "Use an alternative port for the REST api", int],
+    ]
+    optFlags = [
+    ]
+
+
+config = {
+    'address': '0.0.0.0',
+    'port': 8090,
+    'keys': [
+        {
+            'alias': "my peer",
+            'generation': u"medium",
+            'file': u"ec.pem"
+        }
+    ],
+    'logger': {
+        'level': "ERROR"
+    },
+    'walker_interval': 0.5,
+    'overlays': [
+        {
+            'class': 'DiscoveryCommunity',
+            'key': "my peer",
+            'walkers': [
+                {
+                    'strategy': "RandomWalk",
+                    'peers': -1,
+                    'init': {
+                        'timeout': 3.0
+                    }
+                },
+                {
+                    'strategy': "RandomChurn",
+                    'peers': -1,
+                    'init': {
+                        'sample_size': 64,
+                        'ping_interval': 1.0,
+                        'inactive_time': 1.0,
+                        'drop_time': 3.0
+                    }
+                }
+            ],
+            'initialize': {},
+            'on_start': [
+                ('resolve_dns_bootstrap_addresses', )
+            ]
+        },
+    ]
+}
+
+
+class DappCrowdServiceMaker(object):
+    implements(IServiceMaker, IPlugin)
+    tapname = "dappcrowd"
+    description = "Dappcrowd service"
+    options = Options
+
+    def __init__(self):
+        self.ipv8 = None
+        self.restapi = None
+        self._stopping = False
+
+    def start(self, options):
+        """
+        Main method to startup the DAppCrowd service.
+        """
+        root = logging.getLogger()
+        root.setLevel(logging.INFO)
+
+        stderr_handler = logging.StreamHandler(sys.stderr)
+        stderr_handler.setLevel(logging.INFO)
+        stderr_handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(message)s"))
+        root.addHandler(stderr_handler)
+
+        if options["statedir"]:
+            # If we use a custom state directory, update various variables
+            for key in config["keys"]:
+                key["file"] = os.path.join(options["statedir"], key["file"])
+
+        self.ipv8 = IPv8(config)
+
+        # Load TrustChain + DAppCrowd community
+        my_peer = self.ipv8.overlays[0].my_peer
+
+        tc_settings = TrustChainSettings()
+        tc_settings.crawler = True
+        dappcrowd_tc_community = DAppCrowdTrustchainCommunity(my_peer, self.ipv8.endpoint, self.ipv8.network,
+                                                              settings=tc_settings,
+                                                              working_directory=options["statedir"])
+        self.ipv8.overlays.append(dappcrowd_tc_community)
+        self.ipv8.strategies.append((RandomWalk(dappcrowd_tc_community), 20))
+
+        dappcrowd_community = DAppCrowdCommunity(my_peer, self.ipv8.endpoint, self.ipv8.network)
+        self.ipv8.overlays.append(dappcrowd_community)
+        self.ipv8.strategies.append((RandomWalk(dappcrowd_community), 20))
+
+        def signal_handler(sig, _):
+            msg("Received shut down signal %s" % sig)
+            if not self._stopping:
+                self._stopping = True
+                if self.restapi:
+                    self.restapi.stop()
+                self.ipv8.stop()
+
+        signal.signal(signal.SIGINT, signal_handler)
+        signal.signal(signal.SIGTERM, signal_handler)
+
+        msg("Starting DAppCrowd")
+
+        self.restapi = RESTManager(self.ipv8)
+        reactor.callLater(0.0, self.restapi.start, options["apiport"])
+
+    def makeService(self, options):
+        """
+        Construct a IPv8 service.
+        """
+        crawler_service = MultiService()
+        crawler_service.setName("DAppCrowd")
+
+        reactor.callWhenRunning(self.start, options)
+
+        return crawler_service
+
+
+service_maker = DappCrowdServiceMaker()
